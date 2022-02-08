5b44032df304380373678b23a0ae67ab9e1131ee
DavinderSinghKharoud <dskharoud2@gmail.com>
2022-02-07 21:49:37 -0500

    When expression

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,41 @@
+import kotlin.math.abs
 
 fun main() {
-    print("Hello world")
+    whenExpression()
+}
 
-    var sentence = "I'm a sentence"
-    val pi = 1.9F
-    val number: Int = 123
+fun whenExpression() {
+    var burgersOrdered = 2
+    when (burgersOrdered) {
+        0 -> println("Not hungry")
+        1, 2 -> println("Hungry")
+        3 -> println("Very hungry")
+        else -> println("Are you sure")
+    }
 
-    ifExpression()
+    burgersOrdered = -1
+    when (burgersOrdered) {
+        abs(burgersOrdered) -> println("Orders 0 or more burgers please")
+        else -> {
+            println("Ordered less than 0")
+        }
+    }
+
+    when (burgersOrdered) {
+        0 -> println("We need orders")
+        in 1..4 -> println("Got some orders")
+        in 5..9 -> println("Businees is up")
+        else -> {
+            println("Not sure")
+        }
+    }
+
+    when {
+        burgersOrdered <= 0 -> println("None ordered")
+        burgersOrdered % 2 == 1 -> println("Odd number ordered")
+        burgersOrdered % 2 == 0 -> println("Even number ordered")
+    }
+
 }
 
 fun ifExpression(){

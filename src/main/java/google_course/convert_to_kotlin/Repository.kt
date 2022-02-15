f08cd26e82eaa93d2460938e35dfac2312642842
DavinderSinghKharoud <dskharoud2@gmail.com>
2022-02-14 21:37:11 -0500

    Backing property

--- src/main/java/google_course/convert_to_kotlin/Repository.kt
+++ src/main/java/google_course/convert_to_kotlin/Repository.kt
@@ -2,24 +2,29 @@ package google_course.convert_to_kotlin
 
 //Singleton
 object Repository {
-    private var users = mutableListOf<User>()
-    fun getUsers(): List<User> {
-        return users
-    }
+    private val _users = mutableListOf<User>() //The convention for backing properties is to use a leading underscore.
+
+    /**
+     * With the above change, the private _users property becomes the backing property for the public user property.
+     * Outside of the Repository class, the _users list is not modifiable, as consumers of the class can access the list only through users.
+     */
+    val users: List<User>
+        get() = _users
+
 
     val formattedUserNames: List<String>
         get() {
-            return users.map { user ->
-                val name: String = if (user.lastName != null) {
+            return _users.map { user ->
+                if (user.lastName != null) {
                     if (user.firstName != null) {
                         "$user.firstName $user.lastName"
                     } else {
                         user.lastName ?: "Unknown"
                     }
-                } else
-                    user.firstName ?: "Unknown" //Elvis operator: If variable is not null it will be returned else variable to the right
-                name
-            }.toList()
+                } else {
+                    user.firstName ?: "Unknown"
+                } //Elvis operator: If variable is not null it will be returned else variable to the right
+            }
         }
 
     //companion object {
@@ -29,8 +34,8 @@ object Repository {
         val user1 = User("Jane", "")
         val user2 = User("John", null)
         val user3 = User("Anne", "Doe")
-        users.add(user1)
-        users.add(user2)
-        users.add(user3)
+        _users.add(user1)
+        _users.add(user2)
+        _users.add(user3)
     }
 }
